6d0081c2b1edbadb68d26c5c9bb695d3498d5084
Vixsa <victoriagiter@gmail.com>
2023-02-24 17:06:45 +0200

    третья задача

--- zada4a3/Program.cs
+++ zada4a3/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите N:");
+int N = int.Parse(Console.ReadLine());
+int[] result = new int[N];
+for (int i = 1; i <= N; i++)
+result[i-1] = i*i*i;
+for (int i = 0; i <N; i++)
+Console.Write($"{result[i]} ");
