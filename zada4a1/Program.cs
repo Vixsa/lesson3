1247db4c248fd30611b8276c499bc3c2f10ee612
Vixsa <victoriagiter@gmail.com>
2023-02-24 18:40:37 +0200

    первое задание

---
+++ zada4a1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите N: ");
+string N = Console.ReadLine();
+int[] array = new int[5];
+if (N.Length == 5)
+{
+if (N[0] == N[4] || N[1] == N[3])
+{
+    Console.Write("Число является палиндромом");
+    } 
+else 
+{
+    Console.Write("Число не является палиндромом");
+    }}
+
+else
+{
+    Console.Write("Введите пятизначное число");
+}
