20b6cf3a32aa199219a3a16bff15e21df070b265
Vixsa <123653123+Vixsa@users.noreply.github.com>
2023-02-25 11:42:10 +0200

    Update Program.cs

--- zada4a1/Program.cs
+++ zada4a1/Program.cs
@@ -3,7 +3,7 @@ string N = Console.ReadLine();
 int[] array = new int[5];
 if (N.Length == 5)
 {
-if (N[0] == N[4] || N[1] == N[3])
+if (N[0] == N[4] && N[1] == N[3])
 {
     Console.Write("Число является палиндромом");
     } 
